e743344bd3e839f02f778b096683c8e0f8a32777
Matt Schreiber <EMAIL>
2015-12-11 08:53:53 -0500

    fixed test for wrong exec path -- chruby => chruby-exec and also unset executable bit on 90_direnv.sh

--- source/30_ruby.sh
+++ source/30_ruby.sh
@@ -7,8 +7,9 @@ GEM_HOME="$(ruby -rubygems -e 'print Gem.user_dir')"
 export GEM_HOME
 eval "$(path_unshift "${GEM_HOME}/bin")"
 
-if exists chruby ; then
+if exists chruby-exec; then
     for chruby_env in /usr/share/chruby/{chruby,auto}.sh; do
+        # shellcheck disable=SC1090
         [[ -f "$chruby_env" ]] && source "$chruby_env"
     done
 fi
