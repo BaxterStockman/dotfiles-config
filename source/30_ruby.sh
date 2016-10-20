288ca7d07c84cd2842646432a71e386dd023e536
Matt Schreiber <EMAIL>
2016-10-20 06:32:00 -0400

    Removed definition of GEM_HOME as we'll use chruby to do the heavy lifting instead

--- source/30_ruby.sh
+++ source/30_ruby.sh
@@ -3,10 +3,6 @@
 # Make local gems available
 exists ruby && exists gem || return
 
-GEM_HOME="$(ruby -rubygems -e 'print Gem.user_dir')"
-export GEM_HOME
-eval "$(path_unshift "${GEM_HOME}/bin")"
-
 if exists chruby-exec; then
     for chruby_env in /usr/share/chruby/{chruby,auto}.sh; do
         # shellcheck disable=SC1090
